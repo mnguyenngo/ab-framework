c55b40380317dd4727296d4fbe9e5eaf249ac0db
Nguyen Ngo <mnguyenngo@gmail.com>
2018-07-24 16:26:11 -0700

    Add functionality to shade power, alpha, or beta

--- src/stats.py
+++ src/stats.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as scs
 
 
 def pooled_prob(N_A, N_B, X_A, X_B):
@@ -11,3 +12,52 @@ def pooled_SE(N_A, N_B, X_A, X_B):
     p_hat = pooled_prob(N_A, N_B, X_A, X_B)
     SE = np.sqrt(p_hat * (1 - p_hat) * (1 / N_A + 1 / N_B))
     return SE
+
+
+def confidence_interval(sample_mean=0, sample_std=1, sample_size=1,
+                        sig_level=0.05):
+    """Returns the confidence interval as a tuple"""
+    z = z_val(sig_level)
+
+    left = sample_mean - z * sample_std / np.sqrt(sample_size)
+    right = sample_mean + z * sample_std / np.sqrt(sample_size)
+
+    return (left, right)
+
+
+def z_val(sig_level=0.05, two_tailed=True):
+    """Returns the z value for a given significance level"""
+    z_dist = scs.norm()
+    if two_tailed:
+        sig_level = sig_level/2
+        area = 1 - sig_level
+    else:
+        area = 1 - sig_level
+
+    z = z_dist.ppf(area)
+
+    return z
+
+
+def ab_dist(stderr, d_hat=0, group_type='control'):
+    """Returns a distribution object depending on group type
+
+    Examples:
+
+    Parameters:
+        stderr (float): pooled standard error of two independent samples
+        d_hat (float): the mean difference between two independent samples
+        group_type (string): 'control' and 'test' are supported
+
+    Returns:
+        dist (scipy.stats distribution object)
+    """
+    if group_type == 'control':
+        sample_mean = 0
+
+    elif group_type == 'test':
+        sample_mean = d_hat
+
+    # create a normal distribution which is dependent on mean and std dev
+    dist = scs.norm(sample_mean, stderr)
+    return dist
