d4009d1116b99f88388fcac69e3c78cc6d3d5713
Nguyen Ngo <mnguyenngo@gmail.com>
2018-08-05 14:23:42 -0700

    Modified arguments to generate data

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@ import pandas as pd
 # import numpy as np
 
 
-def generate_data(N_A, N_B, bcr, d_hat, days=None, control_label='A',
+def generate_data(N_A, N_B, p_A, p_B, days=None, control_label='A',
                   test_label='B'):
     """Returns a pandas dataframe with fake CTR data
 
@@ -14,11 +14,8 @@ def generate_data(N_A, N_B, bcr, d_hat, days=None, control_label='A',
         N_B (int): sample size for test group
             Note: final sample size may not match N_A provided because the
             group at each row is chosen at random (50/50).
-        bcr (float): base conversion rate; conversion rate of control
-        d_hat (float): difference in conversion rate between the control and
-            test groups, sometimes referred to as **minimal detectable effect**
-            when calculating minimum sample size or **lift** when discussing
-            positive improvement desired from launching a change.
+        p_A (float): conversion rate; conversion rate of control group
+        p_B (float): conversion rate; conversion rate of test group
         days (int): optional; if provided, a column for 'ts' will be included
             to divide the data in chunks of time
             Note: overflow data will be included in an extra day
@@ -46,11 +43,11 @@ def generate_data(N_A, N_B, bcr, d_hat, days=None, control_label='A',
                 raise ValueError("Provide an integer for the days parameter.")
         # assign group based on 50/50 probability
         row['group'] = scs.bernoulli(0.5).rvs()
-        if row['group'] == 1:
+        if row['group'] == 0:
             # assign conversion based on provided parameters
-            row['converted'] = scs.bernoulli(bcr+d_hat).rvs()
+            row['converted'] = scs.bernoulli(p_A).rvs()
         else:
-            row['converted'] = scs.bernoulli(bcr).rvs()
+            row['converted'] = scs.bernoulli(p_B).rvs()
         # collect row into data container
         data.append(row)
 
